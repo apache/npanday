e21a27a7d31645014c9945fa1b7956503b614207
Britton Isbell <sisbell@apache.org>
2007-05-20 22:09:50 +0000

    Initial import of an archetype for a simple .NET project.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nmaven/trunk@539959 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archetypes/maven-archetype-dotnet-simple/src/main/resources/archetype-resources/src/main/csharp/Sample/MyApp.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace Sample
+{
+    public class MyApp
+    {
+        public MyApp()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
