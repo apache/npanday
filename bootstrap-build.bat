479c7f207085a130859e08a0b02996f12fb13b0f
Britton Isbell <sisbell@apache.org>
2007-05-26 02:12:52 +0000

    Working bootstrap build for windows.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nmaven/trunk@541845 13f79535-47bb-0310-9956-ffa450edef68

--- bootstrap-build.bat
+++ bootstrap-build.bat
@@ -6,31 +6,20 @@ call mvn %phase%
 IF errorlevel 1 GOTO END
 
 ECHO Installing 3rd Party Assemblies in the Local Repo
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=dll -Dversion=2.2.8.0
+call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=dll -DartifactVersion=2.2.8.0
 IF errorlevel 1 GOTO END
 
 if "%1" == "-DMicrosoft" (
     ECHO Compiling Assemblies with Microsoft
-    call mvn -f ./assemblies/pom.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MICROSOFT %phase% %*
+    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MICROSOFT %phase% %*
     IF errorlevel 1 GOTO END
-
-    call mvn -f ./plugins/pom-netplugins.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MICROSOFT %phase% %*
-    IF errorlevel 1 GOTO END
-
  ) else  if "%1" == "-DMono" (
     ECHO Compiling Assemblies with Mono
-    call mvn -f ./assemblies/pom.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MONO %phase% %*
+    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MONO %phase% %*
     IF errorlevel 1 GOTO END
-
-    call mvn -f ./plugins/pom-netplugins.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MONO %phase% %*
-    IF errorlevel 1 GOTO END
-
  ) else (
     ECHO Compiling Assemblies with Unknown Vendor
-    call mvn -f ./assemblies/pom.xml -Dmaven.test.skip=true -Dbootstrap %phase% %*
-    IF errorlevel 1 GOTO END
-
-    call mvn -f ./plugins/pom-netplugins.xml -Dmaven.test.skip=true -Dbootstrap %phase% %*
+    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap %phase% %*
     IF errorlevel 1 GOTO END
  )
 
