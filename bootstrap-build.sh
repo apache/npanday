9a695fa520c3c5f185c902496dc38b019a5e47f5
Britton Isbell <sisbell@apache.org>
2007-05-26 02:19:58 +0000

    Updated build for linux.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nmaven/trunk@541847 13f79535-47bb-0310-9956-ffa450edef68

--- bootstrap-build.sh
+++ bootstrap-build.sh
@@ -1,8 +1,3 @@
 mvn install $*
-mvn -f ./plugins/NMaven.Plugin.Addin/pom-java.xml install $*
-mvn -f ./plugins/NMaven.Plugin.Devenv/pom-java.xml install $*
-mvn -f ./plugins/NMaven.Plugin.Settings/pom-java.xml install $*
-mvn -f ./plugins/NMaven.Plugin.Solution/pom-java.xml install $*
-mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=dll -Dversion=2.2.8.0
-mvn -f ./assemblies/pom.xml -Dmaven.test.skip=true -Dbootstrap install $*
-mvn -f ./plugins/pom-netplugins.xml -Dmaven.test.skip=true -Dbootstrap install $*
+mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=dll -DartifactVersion=2.2.8.0
+mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap install $*
