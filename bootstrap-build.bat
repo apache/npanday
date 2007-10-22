b9ac09da6fc3f7020de781a8376a4d3c2bfe0e72
Britton Isbell <sisbell@apache.org>
2007-10-22 18:05:45 +0000

    Chnaged script to run under continuum.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nmaven/trunk@587174 13f79535-47bb-0310-9956-ffa450edef68

--- bootstrap-build.bat
+++ bootstrap-build.bat
@@ -2,14 +2,14 @@
 IF "%phase%"=="" SET phase=install
 ECHO Executing Phase: %phase%
 
-call mvn install:install-file -Dfile=./thirdparty/org.apache.ws/XmlSchema-1.1.jar -DpomFile=./thirdparty/org.apache.ws/XmlSchema-1.1.pom -DgroupId=org.apache.ws.commons -DartifactId=XmlSchema -Dversion=1.1
+cmd /C mvn.bat install:install-file -Dfile=./thirdparty/org.apache.ws/XmlSchema-1.1.jar -DpomFile=./thirdparty/org.apache.ws/XmlSchema-1.1.pom -DgroupId=org.apache.ws.commons -DartifactId=XmlSchema -Dversion=1.1
 if "%1"=="-DrdfProfile.none" (
     ECHO Building Without RDF Profile
-    call mvn %phase% %*
+    cmd /C mvn.bat %phase% %*
 
  ) else (
     ECHO Building With RDF Profile
-    call mvn %phase% -DRdf %*
+    cmd /C mvn.bat %phase% -DRdf %*
  )
 
 :END
@@ -18,24 +18,24 @@ IF errorlevel 1 GOTO END
 
 ECHO Installing 3rd Party Assemblies in the Local Repo
 rem call mvn install:install-file -Dfile=./thirdparty/org.apache.ws/XmlSchema-1.1.jar -DpomFile=./thirdparty/org.apache.ws/XmlSchema-1.1.pom -DgroupId=org.apache.ws.commons -DartifactId=XmlSchema -Dversion=1.1
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=library -DartifactVersion=2.2.8.0
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.Core.dll -DpomFile=./thirdparty/Castle/Castle.Core-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.Core -Dpackaging=library -DartifactVersion=2.0-rc2
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.DynamicProxy.dll -DpomFile=./thirdparty/Castle/Castle.DynamicProxy-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.DynamicProxy -Dpackaging=library -DartifactVersion=2.0-rc2
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.MicroKernel.dll -DpomFile=./thirdparty/Castle/Castle.MicroKernel-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.MicroKernel -Dpackaging=library -DartifactVersion=2.0-rc2
-call mvn org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.Windsor.dll -DpomFile=./thirdparty/Castle/Castle.Windsor-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.Windsor -Dpackaging=library -DartifactVersion=2.0-rc2
+cmd /C mvn.bat org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/NUnit/NUnit.Framework.dll -DgroupId=NUnit -DartifactId=NUnit.Framework -Dpackaging=library -DartifactVersion=2.2.8.0
+cmd /C mvn.bat org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.Core.dll -DpomFile=./thirdparty/Castle/Castle.Core-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.Core -Dpackaging=library -DartifactVersion=2.0-rc2
+cmd /C mvn.bat org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.DynamicProxy.dll -DpomFile=./thirdparty/Castle/Castle.DynamicProxy-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.DynamicProxy -Dpackaging=library -DartifactVersion=2.0-rc2
+cmd /C mvn.bat org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.MicroKernel.dll -DpomFile=./thirdparty/Castle/Castle.MicroKernel-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.MicroKernel -Dpackaging=library -DartifactVersion=2.0-rc2
+cmd /C mvn.bat org.apache.maven.dotnet.plugins:maven-install-plugin:install-file -Dfile=./thirdparty/Castle/Castle.Windsor.dll -DpomFile=./thirdparty/Castle/Castle.Windsor-2.0-rc2.pom -DgroupId=Castle -DartifactId=Castle.Windsor -Dpackaging=library -DartifactVersion=2.0-rc2
 IF errorlevel 1 GOTO END
 
 if "%1" == "-DMicrosoft" (
     ECHO Compiling Assemblies with Microsoft
-    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MICROSOFT %phase% %*
+    cmd /C mvn.bat -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MICROSOFT %phase% %*
     IF errorlevel 1 GOTO END
  ) else  if "%1" == "-DMono" (
     ECHO Compiling Assemblies with Mono
-    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MONO %phase% %*
+    cmd /C mvn.bat -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap -Dvendor=MONO %phase% %*
     IF errorlevel 1 GOTO END
  ) else (
     ECHO Compiling Assemblies with Unknown Vendor
-    call mvn -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap %phase% %*
+    cmd /C mvn.bat -f pom-dotnet.xml -Dmaven.test.skip=true -Dbootstrap %phase% %*
     IF errorlevel 1 GOTO END
  )
 
