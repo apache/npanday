eada3a5ba0ea58e63ed8bbc422725dd5741b9a83
Britton Isbell <sisbell@apache.org>
2007-12-13 10:55:44 +0000

    Missed an add on the unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/nmaven/trunk@603880 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archetypes/maven-archetype-dotnet-simple/src/main/resources/archetype-resources/src/test/csharp/Sample/MyAppTest.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+
+namespace Sample
+{
+
+	[TestFixture]
+	public class MyAppTest
+	{
+
+		[SetUp]
+		protected void SetUp()
+		{
+		}
+
+		[Test]
+		public void TestSample()
+		{
+
+		}
+	}
+}
+
