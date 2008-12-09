a6f3e0258a016dfeb30978879312cce333dc7849
SND\lagdeppa_cp <dev-null@apache.org>
2008-12-09 08:27:37 +0000

    Rebrand Commit, From NMaven To NPanday Except Assemblies folder
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@993820 13f79535-47bb-0310-9956-ffa450edef68

--- components/dotnet-artifact/src/test/java/org/apache/maven/dotnet/artifact/impl/ArtifactInstallerImplTest.java
+++ components/dotnet-artifact/src/test/java/org/apache/maven/dotnet/artifact/impl/ArtifactInstallerImplTest.java
@@ -41,12 +41,12 @@ public class ArtifactInstallerImplTest
         artifactInstaller.init( null, new ArrayList<ArtifactRepository>(), testRepo );
         ArtifactFactory stub = new ArtifactFactoryTestStub();
         artifactInstaller.initTest( stub, new DummyLogger() );
-        Artifact artifact = stub.createArtifact( "NMaven.Model", "NMaven.Model.Pom", "1.0", "compile", "library" );
+        Artifact artifact = stub.createArtifact( "NPanday.Model", "NPanday.Model.Pom", "1.0", "compile", "library" );
         artifact.setFile( new File( System.getProperty( "basedir" ),
-                                    "target/test-repo/uac/gac_msil/NMaven.Model.Pom/1.0__NMaven.Model/NMaven.Model.Pom.dll" ) );
+                                    "target/test-repo/uac/gac_msil/NPanday.Model.Pom/1.0__NPanday.Model/NPanday.Model.Pom.dll" ) );
         Dependency dependency = new Dependency();
-        dependency.setGroupId( "NMaven" );
-        dependency.setArtifactId( "NMaven.Test" );
+        dependency.setGroupId( "NPanday" );
+        dependency.setArtifactId( "NPanday.Test" );
         dependency.setVersion( "1.0" );
         dependency.setType( "library" );
 
@@ -63,8 +63,8 @@ public class ArtifactInstallerImplTest
         }
 
         assertTrue( "Could not find main artifact", new File(
-            "target/test-repo/pab/gac_msil/NMaven.Model.Pom/1.0__NMaven.Model/NMaven.Model.Pom.dll" ).exists() );
+            "target/test-repo/pab/gac_msil/NPanday.Model.Pom/1.0__NPanday.Model/NPanday.Model.Pom.dll" ).exists() );
         assertTrue( "Could not find dependent artifact", new File(
-            "target/test-repo/pab/gac_msil/NMaven.Model.Pom/1.0__NMaven.Model/NMaven.Test.dll" ).exists() );
+            "target/test-repo/pab/gac_msil/NPanday.Model.Pom/1.0__NPanday.Model/NPanday.Test.dll" ).exists() );
     }
 }
