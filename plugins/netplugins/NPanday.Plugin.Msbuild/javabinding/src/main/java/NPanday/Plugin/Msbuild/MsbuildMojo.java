2ba6fdfe8462096b43deb799322a33ed8627740a
Brett Leslie Porter <brett@apache.org>
2010-02-04 08:15:12 +0000

    [#11695] copy references into .references for msbuild
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@994394 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/netplugins/NPanday.Plugin.Msbuild/javabinding/src/main/java/NPanday/Plugin/Msbuild/MsbuildMojo.java
+++ plugins/netplugins/NPanday.Plugin.Msbuild/javabinding/src/main/java/NPanday/Plugin/Msbuild/MsbuildMojo.java
@@ -19,110 +19,161 @@
 
 package NPanday.Plugin.Msbuild;
 
+import java.io.File;
+import java.io.IOException;
+
 import npanday.plugin.FieldAnnotation;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * @phase validate
  * @goal compile
+ * @requiresDependencyResolution test
  */
 public class MsbuildMojo
     extends npanday.plugin.AbstractMojo
 {
-       /**
-        * @parameter expression = "${settings.localRepository}"
-        */
-        @FieldAnnotation()
-        public java.lang.String repository;
+    /**
+     * @parameter expression = "${settings.localRepository}"
+     */
+    @FieldAnnotation()
+    public java.lang.String repository;
 
-       /**
-        * @parameter expression = "${project}"
-        */
-        @FieldAnnotation()
-        public org.apache.maven.project.MavenProject mavenProject;
+    /**
+     * @parameter expression = "${project}"
+     */
+    @FieldAnnotation()
+    public org.apache.maven.project.MavenProject mavenProject;
 
-       /**
-        * @parameter expression = "${project}"
-        */
-        private org.apache.maven.project.MavenProject project;
+    /**
+     * @parameter expression = "${project}"
+     */
+    private org.apache.maven.project.MavenProject project;
 
-       /**
-        * @parameter expression = "${settings.localRepository}"
-        */
-        private String localRepository;
+    /**
+     * @parameter expression = "${settings.localRepository}"
+     */
+    private String localRepository;
 
-       /**
-        * @parameter expression = "${vendor}"
-        */
-        private String vendor;
+    /**
+     * @parameter expression = "${vendor}"
+     */
+    private String vendor;
 
-       /**
-        * @parameter expression = "${vendorVersion}"
-        */
-        private String vendorVersion;
+    /**
+     * @parameter expression = "${vendorVersion}"
+     */
+    private String vendorVersion;
 
-       /**
-        * @parameter expression = "${frameworkVersion}"
-        */
-        private String frameworkVersion;
+    /**
+     * @parameter expression = "${frameworkVersion}"
+     */
+    private String frameworkVersion;
 
-       /**
-        * @component
-        */
-        private npanday.executable.NetExecutableFactory netExecutableFactory;
+    /**
+     * @component
+     */
+    private npanday.executable.NetExecutableFactory netExecutableFactory;
 
-       /**
-        * @component
-        */
-        private npanday.plugin.PluginContext pluginContext;
+    /**
+     * @component
+     */
+    private npanday.plugin.PluginContext pluginContext;
 
-        public String getMojoArtifactId()
+    /**
+     * @parameter default-value=".references"
+     */
+    private File referencesDirectory;
+
+    /**
+     * @parameter default-value="true"
+     */
+    private boolean copyReferences = true;
+
+    public String getMojoArtifactId()
+    {
+        return "NPanday.Plugin.Msbuild";
+    }
+
+    public String getMojoGroupId()
+    {
+        return "npanday.plugin";
+    }
+
+    public String getClassName()
+    {
+        return "NPanday.Plugin.Msbuild.MsbuildMojo";
+    }
+
+    public npanday.plugin.PluginContext getNetPluginContext()
+    {
+        return pluginContext;
+    }
+
+    public npanday.executable.NetExecutableFactory getNetExecutableFactory()
+    {
+        return netExecutableFactory;
+    }
+
+    public org.apache.maven.project.MavenProject getMavenProject()
+    {
+        return project;
+    }
+
+    public String getLocalRepository()
+    {
+        return localRepository;
+    }
+
+    public String getVendorVersion()
+    {
+        return vendorVersion;
+    }
+
+    public String getVendor()
+    {
+        return vendor;
+    }
+
+    public String getFrameworkVersion()
+    {
+        return frameworkVersion;
+    }
+
+    @Override
+    public boolean preExecute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        if ( copyReferences )
         {
-            return "NPanday.Plugin.Msbuild";
+            for ( Object artifact : project.getDependencyArtifacts() )
+            {
+                Artifact a = (Artifact) artifact;
+
+                String path = a.getGroupId() + "/" + a.getFile().getName() + "/" + a.getArtifactId() + "." +
+                    a.getArtifactHandler().getExtension();
+                File targetFile = new File( referencesDirectory, path );
+
+                if ( !targetFile.exists() )
+                {
+                    targetFile.getParentFile().mkdirs();
+
+                    try
+                    {
+                        FileUtils.copyFile( a.getFile(), targetFile );
+                    }
+                    catch ( IOException e )
+                    {
+                        throw new MojoExecutionException(
+                            "Error copying reference from the local repository to .references: " + e.getMessage(), e );
+                    }
+                }
+            }
         }
 
-        public String getMojoGroupId()
-        {
-            return "npanday.plugin";
-        }
-
-        public String getClassName()
-        {
-            return "NPanday.Plugin.Msbuild.MsbuildMojo";
-        }
-
-        public npanday.plugin.PluginContext getNetPluginContext()
-        {
-            return pluginContext;
-        }
-
-        public npanday.executable.NetExecutableFactory getNetExecutableFactory()
-        {
-            return netExecutableFactory;
-        }
-
-        public org.apache.maven.project.MavenProject getMavenProject()
-        {
-            return project;
-        }
-
-        public String getLocalRepository()
-        {
-            return localRepository;
-        }
-
-        public String getVendorVersion()
-        {
-            return vendorVersion;
-        }
-
-        public String getVendor()
-        {
-            return vendor;
-        }
-
-        public String getFrameworkVersion()
-        {
-            return frameworkVersion;
-        }
-
+        return super.preExecute();
+    }
 }
