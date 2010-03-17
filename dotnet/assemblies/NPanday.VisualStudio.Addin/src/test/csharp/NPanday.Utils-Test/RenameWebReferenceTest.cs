0e6e0a7cc4aae4985f69076bc75e3272f0078f65
Adelita L. Padilla <apadilla@apache.org>
2010-03-17 08:41:38 +0000

    revised unit test for NPanday-11711.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@994567 13f79535-47bb-0310-9956-ffa450edef68

--- dotnet/assemblies/NPanday.VisualStudio.Addin/src/test/csharp/NPanday.Utils-Test/RenameWebReferenceTest.cs
+++ dotnet/assemblies/NPanday.VisualStudio.Addin/src/test/csharp/NPanday.Utils-Test/RenameWebReferenceTest.cs
@@ -10,20 +10,22 @@ namespace ConnectTest.UtilsTest
     [TestFixture]
     public class RenameWebReferenceTest
     {
-        private PomHelperUtility pom;
         private PomHelperUtility pomCopy;
         private String pomPath;
         private String pomCopyPath;
         private String fullPath;
+        private String fullPathCopy;
         private String path;
         private String output;
         private String oldName = "WebRef";
         private String newName = "WebRef2";
 
+        private StringBuilder strLine;
+        private String line;
+        private StreamReader strm;
+
         public RenameWebReferenceTest()
         {
-            pom = new PomHelperUtility(new FileInfo(Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().LastIndexOf("target")) + "\\src\\test\\resource\\ClassLibrary1\\ClassLibrary1\\pom.xml"));
-
             pomPath = (new FileInfo(Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().LastIndexOf("target")) + "\\src\\test\\resource\\ClassLibrary1\\ClassLibrary1\\pom.xml").FullName);
 
             pomCopyPath = pomPath.Replace("pom.xml", "pomCopy.xml");
@@ -33,16 +35,35 @@ namespace ConnectTest.UtilsTest
             File.Copy(pomPath, pomCopyPath);
 
             fullPath = (new FileInfo(Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().LastIndexOf("target")) + "\\src\\test\\resource\\ClassLibrary1\\ClassLibrary1\\Web References\\WebRef").FullName);
-            fullPath = fullPath.Replace(oldName, newName); 
-            path = "Web References\\WebRef\\demoService.wsdl";
+            fullPathCopy = fullPath.Replace(oldName, newName); 
+            path = "Web References\\" + oldName + "\\demoService.wsdl";
         }
 
         [Test]
         public void RenameExistingWebReferenceTest()
         {
+            int ctr = 0;
+
             pomCopy.RenameWebReference(fullPath, oldName, newName, path, output);
-            pomCopy.HasPlugin("npanday.plugin", "maven-wsdl-plugin");
+
+            strm = new StreamReader(pomCopyPath);
+            strLine = new StringBuilder();
+
+            while ((line = strm.ReadLine()) != null)
+            {
+                strLine.Append(line);
+
+                if (line.ToString().Contains("<webreference>"))
+                {
+                    ctr++;
+                }
+            }
+
+            strm.Close();
             File.Delete(pomCopyPath);
+
+            Assert.AreEqual(1, ctr);
+            Assert.IsFalse(strLine.ToString().Contains("<namespace>WebRef</namespace>"));
         }
 
     }
