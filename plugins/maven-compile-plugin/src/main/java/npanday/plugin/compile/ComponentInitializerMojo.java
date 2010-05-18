02b59afe8cad7cd6067be4c1424fd832b3f34248
Brett Leslie Porter <brett@apache.org>
2010-05-18 14:24:55 +0000

    [#13018] don't include transitive dependencies at compile time
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@994702 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/maven-compile-plugin/src/main/java/npanday/plugin/compile/ComponentInitializerMojo.java
+++ plugins/maven-compile-plugin/src/main/java/npanday/plugin/compile/ComponentInitializerMojo.java
@@ -18,6 +18,8 @@
  */
 package npanday.plugin.compile;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -28,6 +30,9 @@ import npanday.artifact.AssemblyResolver;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * This class initializes and validates the setup.
@@ -78,8 +83,30 @@ public class ComponentInitializerMojo
         try
         {
             assemblyResolver.resolveTransitivelyFor( project, project.getDependencies(),
-                                                     project.getRemoteArtifactRepositories(), localRepository,
-                                                     true );
+                                                     project.getRemoteArtifactRepositories(), localRepository, true );
+
+            // we don't actually want transitive dependencies, so take the resolved set and cull back to the same as the
+            // project specifies. TODO: we should be able to simplify this mojo to use Maven's dependency resolution mechanism if we have a way to resolve the GAC/COM dependencies
+            for ( Iterator i = project.getDependencyArtifacts().iterator(); i.hasNext(); )
+            {
+                Artifact artifact = (Artifact) i.next();
+
+                boolean found = false;
+                for ( Iterator j = project.getDependencies().iterator(); j.hasNext() && !found; )
+                {
+                    Dependency dependency  = (Dependency) j.next();
+                    if ( dependency.getGroupId().equals( artifact.getGroupId() ) && dependency.getArtifactId().equals(
+                        artifact.getArtifactId() ) )
+                    {
+                        found = true;
+                    }
+                }
+
+                if ( !found )
+                {
+                    i.remove();
+                }
+            }
         }
         catch ( java.io.IOException e )
         {
