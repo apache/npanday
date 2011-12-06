69562cac3efbbd06f75841e0ac1dfcd5eb3efb81
Brett Porter <brett@apache.org>
2011-12-06 01:40:05 +0000

    bootstrap file for mono
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@1210740 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+#  Workaround for http://jira.codehaus.org/browse/MNG-1911
+#  The first time you build a new NPanday version, the compile-plugin has
+#  to be installed into the repository already.
+
+set -e
+
+echo ###################################################################
+echo  Bootstrapping the NPanday Compile Plugin
+echo ###################################################################
+
+mvn install -Dbootstrap --projects org.apache.npanday.plugins:maven-compile-plugin --also-make
+
+echo ###################################################################
+echo  SUCCESS! Now you should be able to build using 'mvn install'
+echo ###################################################################
+
