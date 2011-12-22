a51381b3339556135a6ab30f2598e9340772add2
Brett Porter <brett@apache.org>
2011-12-22 10:29:32 +0000

    update copyright headers
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@1222160 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/wix-maven-plugin/src/main/java/npanday/plugin/wix/AbstractWixMojo.java
+++ plugins/wix-maven-plugin/src/main/java/npanday/plugin/wix/AbstractWixMojo.java
@@ -1,8 +1,6 @@
 package npanday.plugin.wix;
 
 /*
- * Copyright ---
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
