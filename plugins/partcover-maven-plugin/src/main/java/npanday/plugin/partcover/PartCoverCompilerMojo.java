b2cc18814252946fffff064e3bf97dd73891c81c
Brett Porter <brett@apache.org>
2011-12-23 04:21:19 +0000

    Merge changes from trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/branches/npanday-1.4.x@1222552 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/partcover-maven-plugin/src/main/java/npanday/plugin/partcover/PartCoverCompilerMojo.java
+++ plugins/partcover-maven-plugin/src/main/java/npanday/plugin/partcover/PartCoverCompilerMojo.java
@@ -1,8 +1,6 @@
 package npanday.plugin.partcover;
 
 /*
- * Copyright 2010
- *
 * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
