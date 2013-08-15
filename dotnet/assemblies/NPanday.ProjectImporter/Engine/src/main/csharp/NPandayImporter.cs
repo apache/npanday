fd817a7e7bf034866f94befa37082edf22cfda50
Brett Porter <brett@apache.org>
2013-08-15 09:59:18 +0000

    adjust warning
    System scope dependencies are now copied.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@1514221 13f79535-47bb-0310-9956-ffa450edef68

--- dotnet/assemblies/NPanday.ProjectImporter/Engine/src/main/csharp/NPandayImporter.cs
+++ dotnet/assemblies/NPanday.ProjectImporter/Engine/src/main/csharp/NPandayImporter.cs
@@ -273,17 +273,14 @@ namespace NPanday.ProjectImporter
             {
                 if (depSearchConfig.CopyToMaven)
                 {
-                    warningMsg += "The following artifacts were copied to the local Maven repository:"
-                         + "\n\t" + string.Join("\n\t", nonPortableReferences.ToArray())
-                         + "\nDeploying the reference to a Repository will make the code portable to other machines.";
+                    warningMsg += "The following artifacts were copied to the local Maven repository:";
                 }
                 else
                 {
-                    warningMsg += "\nThe build may not be portable if local references are used:"
-                         + "\n\t" + string.Join("\n\t", nonPortableReferences.ToArray())
-                         + "\nDeploying the reference to a Repository will make the code portable to other machines." 
-                         + "\n\nNote: artifacts with a system path will not be packaged or used as a runtime dependency by Maven";
+                    warningMsg += "\nThe build may not be portable if local references are used:";
                 }
+                warningMsg += "\n\t" + string.Join("\n\t", nonPortableReferences.ToArray())
+                     + "\nDeploying the reference to a Repository will make the code portable to other machines.";
             }
             return result;
         }
