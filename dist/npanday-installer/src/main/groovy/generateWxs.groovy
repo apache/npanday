222ae9394379ba49887e54dc0074a8e2ebcdf8ff
Brett Porter <brett@apache.org>
2014-02-27 11:09:01 +0000

    Add support for VS 2013 in the MSI
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@1572513 13f79535-47bb-0310-9956-ffa450edef68

--- dist/npanday-installer/src/main/groovy/generateWxs.groovy
+++ dist/npanday-installer/src/main/groovy/generateWxs.groovy
@@ -26,7 +26,7 @@ if ( index >= 0 ) {
 def repositoryComponentIds = []
 def repositoryBasedir = new File(project.build.directory + "/repository/releases");
 def generateGuid = { "{"+java.util.UUID.randomUUID().toString().toUpperCase() + "}" }
-def visualStudioVersions = ['2005', '2008', '2010', '2012']
+def visualStudioVersions = ['2005', '2008', '2010', '2012', '2013']
 
 def addinArtifacts = []
 new File(project.build.directory + "/addin").eachFile { addinArtifacts << it }
@@ -62,6 +62,9 @@ def writer = outputFile.withWriter("UTF-8") { writer ->
       }
       Property(Id:"VS2012INSTALLED") {
         RegistrySearch(Id:"VS2012INSTALLED", Root:"HKCR", Key:"VisualStudio.DTE.11.0", Type: "raw")
+      }
+      Property(Id:"VS2013INSTALLED") {
+        RegistrySearch(Id:"VS2013INSTALLED", Root:"HKCR", Key:"VisualStudio.DTE.12.0", Type: "raw")
       }
 
       Condition(Message:"NPanday cannot be installed on Windows 9x/ME", "VersionNT")
