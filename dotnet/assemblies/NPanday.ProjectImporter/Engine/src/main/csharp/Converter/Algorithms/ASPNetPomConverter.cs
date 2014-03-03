56fab82cd05fd80874240ce151fc62d3e440a462
Brett Porter <brett@apache.org>
2014-03-03 09:55:30 +0000

    [NPANDAY-488] Use contentPath by default
    This was the previous behaviour:
    http://svn.apache.org/viewvc/incubator/npanday/trunk/plugins/msdeploy-maven-plugin/src/main/java/npanday/plugin/msdeploy/create/CreateContentPackageMojo.java?r1=1333788&r2=1332286&pathrev=1333788
    
    Also, there is a problem using iisApp with resolve web roles, since it
    expects the contentPath destination:
    http://svn.apache.org/viewvc/incubator/npanday/trunk/plugins/msdeploy-maven-plugin/src/main/java/npanday/plugin/msdeploy/create/CreateContentPackageMojo.java?r1=1333788&r2=1332286&pathrev=1333788
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/npanday/trunk@1573504 13f79535-47bb-0310-9956-ffa450edef68

--- dotnet/assemblies/NPanday.ProjectImporter/Engine/src/main/csharp/Converter/Algorithms/ASPNetPomConverter.cs
+++ dotnet/assemblies/NPanday.ProjectImporter/Engine/src/main/csharp/Converter/Algorithms/ASPNetPomConverter.cs
@@ -54,7 +54,7 @@ namespace NPanday.ProjectImporter.Converter.Algorithms
             }
 
             Plugin msdeployPlugin = AddPlugin("org.apache.npanday.plugins", "msdeploy-maven-plugin", null, false);
-            AddPluginExecution(msdeployPlugin, "create-msdeploy-package", new string[] { "create-iisApp-package" }, null);
+            AddPluginExecution(msdeployPlugin, "create-msdeploy-package", new string[] { "create-content-package" }, null);
 
             if (projectDigest.SilverlightApplicationList != null)
             {
